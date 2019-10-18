b2326b615a711dc63e377ceafd000452220ef6c2
Richard Palethorpe <richiejp@f-m.fm>
2019-10-18 19:33:48 +0200

    Get the initial test working

--- src/luvvie.jl
+++ src/luvvie.jl
@@ -1,20 +1,18 @@
 module luvvie
 
 # Types
-export Id, Actor, Stage, Scene
+export Id, Stage, Scene
 
 # Functions
-export genesis, stage, play!, say, roar, hear, me, my, stage
+export genesis, stage, play!, enter!, leave!, ask, say, roar, hear, me, my,
+       my!, delegate
 
 # Message Types
-export Genesis!, Entered!, Enter!, TheEnd!, Roar!
-
-imap(f) = x -> Iterators.Generator(f, itr)
-ifilter(f) = x -> Iterators.filter(f, x)
+export Genesis!, Entered!, Enter!, Leave!, Roar!, Forward!
 
 # _Naming Conventions_
 #
-# Variable names should be descriptive and obvious with the following common
+# Variable names should be reasonably descriptive with the following
 # exceptions.
 #
 # a   = Actor or an actor ID
@@ -41,7 +39,7 @@ ifilter(f) = x -> Iterators.filter(f, x)
 #
 # _Message types_
 #
-# Types/Structs which are messages have a bang attached (e.g. TheEnd!)
+# Types/Structs which are messages have a bang attached (e.g. Leave!)
 
 mutable struct Actor{S}
     inbox::Channel{Any}
@@ -50,33 +48,38 @@ mutable struct Actor{S}
 end
 
 Actor(data) = Actor(Channel(420), data, nothing)
-my(a::Actor) = a.state
 
 struct Id{S}
     i::UInt64
     ref::Union{Ref{Actor{S}}, Nothing}
 end
 
-function my(a::Id)
+Base.:(==)(a::Id, b::Id) = a.i == b.i
+
+function my_ref(a::Id)
     @assert a.ref !== nothing "Trying to get a remote actor's state"
     @assert a.ref[].task !== nothing "Actor is not playing"
     @assert a.ref[].task === current_task() "Trying to get another actor's state"
 
-    my(a.ref[])
+    a.ref
 end
+
+my(a::Id) = my_ref(a)[].state
+my!(a::Id, state) = my_ref(a)[].state = state
 
 inbox(a::Id) = a.ref[].inbox
 
 Base.show(io::IO, id::Id{S}) where S = print(io, "$S@", id.i)
 
-struct Stage
-    actors::Vector{Id}
+mutable struct Stage
+    actors::Set{Id}
+    props
 end
 
-function genesis()
-    a = Id(UInt64(0), Ref(Actor(Stage(Id[]))))
+function Stage(props=nothing)
+    a = Id(UInt64(0), Ref(Actor(Stage(Set{Id}(), props))))
 
-    put!(a.ref[].inbox, Genesis!())
+    put!(inbox(a), Genesis!())
 
     a
 end
@@ -88,32 +91,79 @@ end
 
 me(s::Scene) = s.subject
 my(s::Scene) = my(me(s))
+my!(s::Scene, state) = my!(me(s), state)
 stage(s::Scene) = s.stage
+inbox(s::Scene) = inbox(me(s))
 
 say(s::Scene, to::Id, msg) = if to.ref === nothing
-    error("Remote addresses not implemented")
+    error("$to appears to be a remote actor; use shout instead")
 else
     @debug "$(stage(s))/$(me(s)) send to $to" msg
     put!(inbox(to), msg)
-    nothing
 end
+
+shout(s::Scene, to::Id, msg) = error("Not implemented")
 
 roar(s::Scene, msg) = say(s, stage(s), Roar!(msg))
 
-hear(::Scene{S}, msg::M) where {S, M} =
-    error("Missing message handler, need hear(::Scene{$S}, ::$M)")
+hear(s::Scene, msg) = hear!(s, msg)
+hear!(::Scene{S}, msg::M) where {S, M} =
+    error("Missing message handler, need hear(!)(::Scene{$S}, ::$M)")
 
-listen!(st::Id{Stage}, a::Id) = for msg in inbox(a)
-    @debug "$st/$a recv" msg
-    s = Scene(a, st)
+function listen!(st::Id{Stage}, a::Id)
+    inb = inbox(a)
+
+    for msg in inb
+        @debug "$st/$a recv" msg
+        s = Scene(a, st)
+
+        try
+            hear(s, msg)
+        catch ex
+            close(inb)
+            showerror(stderr, ex, catch_backtrace())
+        end
+    end
 
     try
-        hear(s, msg)
-    catch ex
-        close(inbox(a))
-        showerror(stderr, ex, catch_backtrace())
+        put!(inbox(st), Left!(a))
+    catch
     end
 end
+
+kill_all!(actors) = for a in actors
+    inb = inbox(a)
+
+    try
+        put!(inb, Leave!())
+    catch
+    end
+
+    close(inb)
+end
+
+function listen!(st::Id{Stage}, ::Id{Stage})
+    inb = inbox(st)
+    as = my(st).actors
+
+    for msg in inb
+        @debug "$st recv" msg
+        s = Scene(st, st)
+
+        try
+            hear(s, msg)
+        catch ex
+            close(inb)
+            kill_all!(as)
+            rethrow()
+        end
+    end
+
+    kill_all!(as)
+    empty!(as)
+end
+
+leave!(s::Scene) = close(inbox(s))
 
 play!(st::Id{Stage}) = play!(st, st)
 
@@ -125,14 +175,47 @@ function play!(st::Id{Stage}, a::Id)
     listen!(st, a)
 end
 
+function enter!(s::Scene{Stage}, actor_state)
+    as = my(s).actors
+    a = Id(UInt64(length(as) + 1), Ref(Actor(actor_state)))
+    push!(as, a)
+
+    st = stage(s)
+    task = Task(() -> play!(st, a))
+    task.sticky = false
+    schedule(task)
+
+    a
+end
+
+function ask(s::Scene, a::Id, favor, ::Type{R}) where R
+    me(s) == a && error("Asking oneself results in deadlock")
+    say(s, a, favor)
+
+    inb = inbox(s)
+    msg = take!(inb)
+    msg isa R && return msg
+
+    scratch = Any[msg]
+    for outer msg in inb
+        msg isa R && break
+
+        push!(scratch, msg)
+    end
+
+    foreach(m -> put!(inb, m), scratch)
+
+    msg
+end
+
 # Messages
 
 struct Genesis! end
 
 hear(::Scene{Stage}, ::Genesis!) = error("You can't ignore Genesis!")
 
-struct Entered!
-    who::Id
+struct Entered!{S}
+    who::Id{S}
 end
 
 struct Enter!
@@ -141,31 +224,56 @@ struct Enter!
 end
 
 function hear(s::Scene{Stage}, msg::Enter!)
-    as = my(s).actors
-    a = Id(UInt64(length(as) + 1), Ref(Actor(msg.actor_state)))
-    push!(as, a)
+    a = enter!(s, msg.actor_state)
 
-    st = stage(s)
-    task = Task(() -> play!(st, a))
-    task.sticky = false
-    schedule(task)
-
-    isnothing(msg.re) || say(s, msg.re, Entered!(a))
+    if isnothing(msg.re)
+        say(s, a, Entered!(a))
+    else
+        say(s, msg.re, Entered!(a))
+    end
 end
 
-struct Roar!
-    msg
+struct Left!
+    who::Id
+end
+
+hear(s::Scene{Stage}, msg::Left!) = delete!(my(s).actors, msg.who)
+
+struct Forward!{T}
+    msg::T
+end
+
+struct Roar!{T}
+    msg::T
 end
 
 hear(s::Scene{Stage}, roar::Roar!) = for a in my(s).actors
-    say(s, a, roar.msg)
+    try
+        say(s, a, roar.msg)
+    catch ex
+        ex isa InvalidStateException || rethrow()
+        @debug "$(me(s)) $a left without saying goodbye!"
+    end
 end
 
-struct TheEnd! end
+struct Leave! end
 
-function hear(s::Scene{Stage}, msg::TheEnd!)
-    hear(s, Roar!(msg))
+hear(s::Scene, msg::Leave!) = close(inbox(s))
+
+# Extras
+
+struct Stooge
+    action::Function
+    args::Tuple
+end
+
+hear(s::Scene{Stooge}, ::Entered!{Stooge}) = let stooge = my(s)
+    stooge.action(s, stooge.args...)
+
     close(inbox(s))
 end
+
+delegate(action::Function, s::Scene, args...) =
+    say(s, stage(s), Enter!(Stooge(action, args), nothing))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,51 +1,71 @@
 using Test
 using luvvie
 
-struct Duck
-    name::String
-    pop::Int
-end
-
-struct WhoLovesMe
-    re::Id
-end
-
-struct HowPopularAreYou
-    re::Id
-end
-
-luvvie.hear(s::Scene{Duck}, msg::HowPopularAreYou) =
-    say(s, msg.re, my(s).pop)
-
-function luvvie.hear(s::Scene{Duck}, msg::WhoLovesMe)
-    other_pop = ask(s, msg.re, HowPopularAreYou(me(s)))
-    my_pop = my(s).pop
-
-    if my_pop <= other_pop
-        say(s, msg.re, Val(:i_love_you))
+# Popularity begets popularity
+#
+# Script:
+#   We create the stage and this triggers Genesis!
+#   In the handler for the Genesis! message we create two actors
+#   One actor is created by sending the Enter! message (Nigel)
+#   The other actor is created inline (Brian)
+#   When Nigel's Enter! message is processed Entered! is sent
+#   In the Entered! handler we ask all the other actors who loves who
+#   Each actor recieves WhoLoves! messages asking if they love another actor
+#   They spawn a Stooge (with delegate()) to query the other's popularity
+#   (if they didn't it could result in deadlock)
+#   If the other actor is more or equally popular, they give them love
+#   Brian is more popular than Nigel so she gets some love and Nigel doesn't
+#   After Brian increases his popularity, he tells the whole Stage to leave
+#   When the Stage recieves the Leave message, it tests Brians popularity
+#   The library then tells all the actors to leave.
+#
+@testset "luvvies sim" begin
+    struct Actor
+        name::String
+        pop::Int
     end
+
+    struct WhoLoves!
+        re::Id
+    end
+
+    struct HowPopularAreYou!
+        re::Id
+    end
+
+    luvvie.hear(s::Scene{Actor}, msg::HowPopularAreYou!) =
+        say(s, msg.re, my(s).pop)
+
+    luvvie.hear(s::Scene{Actor}, msg::WhoLoves!) = if me(s) != msg.re
+        delegate(s, my(s).pop, msg.re) do s, my_pop, re
+            other_pop = ask(s, re, HowPopularAreYou!(me(s)), Int)
+
+            my_pop <= other_pop && say(s, re, Val(:i_love_you!))
+        end
+    end
+
+    luvvie.hear!(s::Scene{Actor}, ::Val{:i_love_you!}) = let state = my(s)
+        my!(s, Actor(state.name, state.pop + 1))
+
+        say(s, stage(s), Leave!())
+    end
+
+    function luvvie.hear(s::Scene{Stage}, msg::Entered!)
+        roar(s, WhoLoves!(msg.who))     # Nigel
+        roar(s, WhoLoves!(my(s).props)) # Brian
+    end
+
+    luvvie.hear(s::Scene{Stage}, ::Genesis!) = let st = stage(s)
+        say(s, st, Enter!(Actor("Nigel", 0), st))
+        my(s).props = enter!(s, Actor("Brian", 1))
+    end
+
+    function luvvie.hear(s::Scene{Stage}, msg::Leave!)
+        @test ask(s, my(s).props, HowPopularAreYou!(me(s)), Int) == 2
+
+        leave!(s)
+    end
+
+    play!(Stage())
 end
-
-function luvvie.hear(s::Scene{Duck}, ::Val{:i_love_you})
-    my(s).pop += 1
-
-    say(stage(s), TheEnd!())
-end
-
-luvvie.hear(s::Scene{Stage}, msg::Entered!) =
-    roar(s, WhoLovesMe(msg.who))
-
-function luvvie.hear(s::Scene{Stage}, ::Genesis!)
-    st = stage(s)
-
-    say(s, st, Enter!(Duck("Nigel", 0), st))
-    say(s, st, Enter!(Duck("Brian", 1), st))
-end
-
-st = genesis()
-
-play!(st)
-
-@test st.ref[].state.actors[1].ref[].state.pop == 2
-
 
